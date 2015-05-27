90e4893f05da98a97bb5b8204322e2ae481a8298
allen wes <allen.wes.g@gmail.com>
2015-05-27 13:06:54 -0500

    changed code to the one aaron did as an example because it was a lot better.. also changed css

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,42 +1,20 @@
-$(document).ready(function(){
-
-	$redTab = $("#red-tab");
-	$greenTab = $("#green-tab");
-	$blueTab = $("#blue-tab");
-
-	$textInfo = $("p > span");
-
-	$redTab.on("click", getRedTabInfo);
-	$greenTab.on("click", getGreenTabInfo);
-	$blueTab.on("click", getBlueTabInfo);
-
-	var infoPara1 = "<p>Duo no choro facilis sensibus, dicam bonorum offendit eum ut. Quis tractatos eum in, elit fabulas dolores mel te, cu eius vidit dignissim qui. Per id assum impetus. Iriure assentior ne mel. Tantas honestatis at duo. Consul democritum efficiendi ex his, ei propriae appetere mea. Cu habeo homero instructior vim, pro no sumo option erroribus.</p>";
-	var infoPara2 = "<p>Duo no choro facilis sensibus, dicam bonorum offendit eum ut. Quis tractatos eum in, elit fabulas dolores mel te, cu eius vidit dignissim qui. Per id assum impetus. Iriure assentior ne mel. Tantas honestatis at duo. Consul democritum efficiendi ex his, ei propriae appetere mea. Cu habeo homero instructior vim, pro no sumo option erroribus.</p>";
-	var infoPara3 = "<p>Duo no choro facilis sensibus, dicam bonorum offendit eum ut. Quis tractatos eum in, elit fabulas dolores mel te, cu eius vidit dignissim qui. Per id assum impetus. Iriure assentior ne mel. Tantas honestatis at duo. Consul democritum efficiendi ex his, ei propriae appetere mea. Cu habeo homero instructior vim, pro no sumo option erroribus.</p>";
-
-
-	function getRedTabInfo(){
-		var title = "<h1>Red</h1>";
-		
-		render(title);
-	}
-
-	function getGreenTabInfo(){
-		var title = "<h1>Green</h1>";
-
-		render(title);
-	}
-
-	function getBlueTabInfo(){
-		var title = "<h1>Blue</h1>";
-
-		render(title);
-	}
-
-	function render(theTitle){
-		var allInfo = theTitle + infoPara2 + infoPara1 + infoPara3;
-
-		$textInfo.html(allInfo)
-	}
+$(document).ready(function(e) {
 	
+	var $targetSection = $(window.location.hash);
+	if(!$targetSection.length) {
+		$targetSection = $('main section').eq(0);
+	}
+	$targetSection.show();
+
+	$('nav a[href="#'+$targetSection.attr('id')+'"]').addClass('active');
+
+	$('nav').on('click', function(e) {
+		var $target = $(e.target);
+		var targetName = $target.attr('href');
+		$('main section').hide();
+		$(targetName).show();
+
+		$('nav a').removeClass('active');
+		$target.addClass('active');
+	});
 });
